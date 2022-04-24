185dfa2703ca88692af443911d847e3666f57b3a
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-04-24 21:39:02 +0530

    Structre the code

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,20 @@
-const express = require("express")
+const express = require("express");
+const { connect } = require("react-redux");
+
+
+const app = express();
+
+
+
+
+app.listen(5000 , ()=>{
+    try{
+
+        await connect();
+        console.log("listening to 5000")
+
+    }
+    catch(e){
+        console.log(e.message);
+    }
+} )
