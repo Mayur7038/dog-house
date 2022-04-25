c10288f88dd9d9a45239a984605a70f0a4d9f0a7
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-04-25 16:58:29 +0530

    houses filter added

--- Backend/src/controller/houses.controller.js
+++ Backend/src/controller/houses.controller.js
@@ -7,10 +7,25 @@ const Houses = require("../model/houses.model");
 
 router.get("" , async(req,res)=>{
 
-    console.log(req)
+
 
     try{
-        const houses = await Houses.find().lean().exec();
+        const houses = await Houses.find(req.query).lean().exec();
+        return res.send(houses);
+    }
+    catch(e){
+        return res.send(e.message);
+    }
+
+})
+
+router.get("/:id" , async(req,res)=>{
+
+    console.log(req.params)
+
+
+    try{
+        const houses = await Houses.findById(req.params.id).lean().exec();
         return res.send(houses);
     }
     catch(e){
@@ -27,6 +42,24 @@ router.post("" , async (req,res)=>{
     }
     catch(e){
         return res.send(e.message);
+    }
+
+})
+
+router.patch("/:id", async(req,res)=>{
+
+
+    
+
+    try{
+
+        const houses = await Houses.findByIdAndUpdate(req.params.id,req.body);
+        return res.send(houses);
+
+    }
+    catch(e){
+        return res.send(e.message);
+        
     }
 
 })
