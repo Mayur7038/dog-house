88528df191528c74258e37edd301e36d28ba9002
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-04-27 15:38:40 +0530

    done

--- Backend/src/controller/houses.controller.js
+++ Backend/src/controller/houses.controller.js
@@ -11,7 +11,6 @@ router.get("" , async(req,res)=>{
 
     try{
         const houses = await Houses.find(req.query).lean().exec();
-        // const houses = await Houses.find().sort({Cost_per_day:-1}).lean().exec();
 
         return res.send(houses);
     }
